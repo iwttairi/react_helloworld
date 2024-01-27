4d4db87b1f186b667267b9e362f2074e12afd840
iwttairi <airiiwtt@gmail.com>
2024-01-27 12:01:01 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -2,22 +2,40 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const greeting = "Hello world JSX!!";
+  const styleObject = {
+    color: 'red',
+    fontSize: '32px',
+  };
+  const containerStyle = {
+    display: 'flex',
+    flexWrap: 'wrap',
+    alignContent: 'center',
+  };
+  const boxStyle = {
+    width: '33%',
+    height: '180px',
+    textAlign: 'center',
+  };
+  const middleBoxStyle = {
+    ...boxStyle,
+    backgroundColor: 'teal',
+  };
+  const colors = ['red', 'green', 'yellow', 'purple', 'orange', 'blue', 'pink', 'gray', 'brown'];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={containerStyle}>
+      <div style={{...boxStyle, backgroundColor: colors[0]}}></div>
+      <div style={{...boxStyle, backgroundColor: colors[1]}}></div>
+      <div style={{...boxStyle, backgroundColor: colors[2]}}></div>
+      <div style={{...boxStyle, backgroundColor: colors[3]}}></div>
+      <div style={middleBoxStyle}>
+        <h1 style={styleObject}>{greeting}</h1>
+        <p>welcome to my react</p>
+      </div>
+      <div style={{...boxStyle, backgroundColor: colors[5]}}></div>
+      <div style={{...boxStyle, backgroundColor: colors[6]}}></div>
+      <div style={{...boxStyle, backgroundColor: colors[7]}}></div>
+      <div style={{...boxStyle, backgroundColor: colors[8]}}></div>
     </div>
   );
 }
